4be14483a2952c62829efa3cb46dcc83a7e0983a
Nicxes <nicxesh@gmail.com>
2024-12-05 00:09:15 -0300

    fix(vercel): possible fix

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: ['https://exbill-api.vercel.app', 'http://localhost:3000'],
+    methods: 'GET, HEAD, PUT, POST, DELETE, OPTIONS, PATCH',
+    credentials: true,
+    allowedHeaders:
+      'Origin, X-Requested-With, Content-Type, Accept, Authentication, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma',
+  });
+
   // Enable global validation with transformation
   app.useGlobalPipes(
     new ValidationPipe({
