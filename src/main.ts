16b50de82854bdeda007da582bfd19a0986dcbeb
Nicxes <nicxesh@gmail.com>
2024-12-04 23:30:39 -0300

    fix(cors): Fixed methods

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
 
